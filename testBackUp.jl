d7c49c49d9f6804411ecb2f96fdb93cf37c17584
emmaRinge <111793635+emmaRinge@users.noreply.github.com>
2022-10-05 00:15:06 -0400

    Update testBackUp.jl

--- testBackUp.jl
+++ testBackUp.jl
@@ -10,19 +10,36 @@ using Plots
 using Polynomials
 using CurveFit
 
-mu1 = rand(0:5)
-sigma1 = rand(1:5)
-mu2 = rand(0:5)
-sigma2 = rand(1:5)
+
+mu1 = 0
+sigma1 = 1
+mu2 = 0
+sigma2 = 1
+muAdd = mu1 + mu2
+sigmaCom = sigma1^2 + sigma2^2
 d = Normal(mu1, sigma1)
 e = Normal(mu2, sigma2)
-x = rand(d, 100)
-y = rand(e, 100)
-z = x .* y
+g = Normal(muAdd, sigmaCom)
+x = rand(d, 10000)
 
-xs = range(0, 10, length = 10)
-ys = @.exp(-xs)
-f2 = poly_fit(xs, ys, 9) # degree = 2
-  
-scatter(xs, ys, markerstrokewidth = 0, label = "Data")
+#Store Moments to condense data
+function storeMoments(data)
+    return [stats.mean(data), stats.std(data)]
+end
+
+#histogram(x)
+
+f2 = @.sin(x)
 plot!(f2)
+
+arr = storeMoments(x)
+h = Normal(arr[1], arr[2])
+y = rand(h, 10000)
+f3 = @.sin(y)
+plot!(f3)
+
+#xs = range(0, 10, length = 10)
+#ys = @.exp(-xs)
+#f2 = poly_fit(xs, ys, 3) # degree = 2
+#scatter(xs, ys, markerstrokewidth = 0, label = "Data")
+#plot!(f2)
