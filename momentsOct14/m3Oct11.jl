026ab9082bf0fbae68a997a953aac9dff91ce6ac
emmaRinge <111793635+emmaRinge@users.noreply.github.com>
2022-11-10 21:33:40 -0500

    Update m3Oct11.jl

--- momentsOct14/m3Oct11.jl
+++ momentsOct14/m3Oct11.jl
@@ -8,25 +8,6 @@ using Cumulants
 using Plots
 using Polynomials
 
-#generate data
-mu1 = rand(0.1:1)
-sigma1 = rand(0.1:1)
-mu2 = rand(0.1:1)
-sigma2 = rand(0.1:1)
-d = Normal(mu1, sigma1)
-e = Normal(mu2, sigma2)
-x = rand(d, 10)
-y = rand(e, 10)
-z = x .* y
-
-xlin = range(findmin(z)[1], findmax(z)[1], length = 10)
-ys = convert(AbstractVector, @.sin(z))
-f = Polynomials.fit(z, ys, 3)
-f2 = Polynomials.fit(z, ys, 6)
-
-scatter(z, ys, markersize=3, label="data")
-plot!(xlin, f.(xlin), label = "f(x) = x3", dpi = 200)
-
 function storeMoments(data)
     sum2 = 0
     sum3 = 0
@@ -50,24 +31,24 @@ function calcPseudomoments(arr)
     m1 = arr[1]
     m2 = arr[2] + (arr[1]^2)
     m3 = arr[3] + (3 * arr[2] * arr[1]) + (arr[1])^3
-    m4 = (4 * arr[3] * arr[1]) 
+    m4 = ((4 * arr[3] * arr[1]) 
         + 3(arr[2])^2 
         + (6 * arr[2] * (arr[1])^2) 
-        + (arr[1])^4
+        + (arr[1])^4)
 
-    m5 = (10 * arr[3] * arr[2]) 
+    m5 = ((10 * arr[3] * arr[2]) 
         + (10 * arr[3] * (arr[1])^2) 
         + (15 * ((arr[2])^2) * arr[1])
         + (10 * arr[2] * (arr[1])^3) 
-        + (arr[1])^5
+        + (arr[1])^5)
 
-    m6 = (10(arr[3])^2) 
+    m6 = ((10(arr[3])^2) 
         + (60 * arr[3] * arr[2] * arr[1]) 
         + (20 * arr[3] * (arr[1])^3)
         + (15 * (arr[2])^3) 
         + (45 * (arr[2])^2 * (arr[1])^2) 
         + (15 * arr[2] * (arr[1])^4)
-        + (arr[1])^6
+        + (arr[1])^6)
     return [m1, m2, m3, m4, m5, m6]
 end
 
@@ -99,14 +80,61 @@ function polyExpEx(fit, arr)
     return sum
 end
 
-arr = storeMoments(z)
-cm = calcCumulants(arr)
-mom = calcPseudomoments(cm)
-println(arr)
-println(mom)
-println(f)
-println(f2)
+function findZ(std)
+    mu1 = rand(0.1:1)
+    sigma1 = rand(0.1:std)
+    mu2 = rand(0.1:1)
+    sigma2 = rand(0.1:std)
+    d = Normal(mu1, sigma1)
+    e = Normal(mu2, sigma2)
+    x = rand(d, 1000)
+    y = rand(e, 1000)
+    z = x .* y
+    return z
+end
 
-println(trueExp(z))
-println(polyExp(f, arr))
-println(polyExpEx(f2, mom))
+function approxZ2(z)
+    #xlin = range(findmin(z)[1], findmax(z)[1], length = 10)
+    #ys = convert(AbstractVector, @.sin(z))
+    x = range(0, 10, length = 10)
+    y = convert(AbstractVector, @.sin(x))
+    return f = Polynomials.fit(x, y, 3)
+end
+function approxZ3(z)
+    #xlin = range(findmin(z)[1], findmax(z)[1], length = 10)
+    #ys = convert(AbstractVector, @.sin(z))
+    x = range(0, 10, length = 10)
+    y = convert(AbstractVector, @.sin(x))
+    return f2 = Polynomials.fit(x, y, 6)
+end
+
+function trialZ(num, std)
+    a = 0
+    b = 0
+    c = 0
+    x = 1
+    data1 = Matrix(undef, num, 4) 
+    while x <= num
+            z = findZ(std)
+            f = approxZ2(z)
+            f2 = approxZ3(z)
+            arr = storeMoments(z)
+            cm = calcCumulants(arr)
+            mom = calcPseudomoments(cm)
+            a = trueExp(z)
+            b = polyExp(f, arr)
+            c = polyExpEx(f2, mom)
+            data1[x, 1] = stats.std(z)
+            data1[x, 2] = a
+            data1[x, 3] = b
+            data1[x, 4] = c
+            a = 0
+            b = 0
+            c = 0
+            x+=1
+        end
+    return data1
+end
+
+df = DataFrame(trialZ(100,1), :auto)
+CSV.write("/Users/emmaringe/m3October11.csv", df)
